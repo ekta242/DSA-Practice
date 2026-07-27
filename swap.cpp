0941c7870ea152127108f0c9c808e9f998a94b80
ekta242 <ektaeben@gmail.com>
2026-07-27 06:33:32 +0530

    Swap  using xor (method 2/3)

--- swap.cpp
+++ swap.cpp
@@ -2,13 +2,22 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+void method1() {
     int a = 10, b = 20;
-  
     a = a + b;
     b = a - b;
     a = a - b;
   
+    cout << a << " " << b << endl;
+    return 0;
+}
+void method2() {
+    int a = 10, b = 20;
+    
+    a = a ^ b;
+    b = a ^ b;
+    a = a ^ b;
+    
     cout << a << " " << b << endl;
     return 0;
 }
