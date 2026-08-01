a8f1d99a266acde0f70edf7fa70e71e0e812443d
ekta242 <ektaeben@gmail.com>
2026-08-01 06:21:20 +0530

    Opposite sides of dice done

---
+++ dice.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    int oppositeFaceOfDice(int n) {
+        switch(n) {
+            case 1: return 6;
+            case 2: return 5;
+            case 3: return 4;
+            case 4: return 3;
+            case 5: return 2;
+            case 6: return 1;
+        }
+    }
+};
