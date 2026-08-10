0c8d81146c688f6c903363528f982e284e6ed756
ekta242 <ektaeben@gmail.com>
2026-08-10 22:38:38 +0530

    Removing duplicate elements in an array

---
+++ Arrays/rmdup.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    vector<int> removeDuplicates(vector<int> &arr) {
+        int n=arr.size();
+        if(n<=1)
+            return arr;
+        int idx=1;
+        for(int i=1;i<n;i++){
+            if(arr[i]!=arr[i-1])
+                arr[idx++]=arr[i];
+        }
+        arr.resize(idx);
+        return arr;
+    }
+};
